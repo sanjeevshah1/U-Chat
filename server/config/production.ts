4aeca5c944ffbfd9d002e75151e3237f827a3820
sanjeevshah1 <sahsanjeev42@gmail.com>
2025-06-20 11:46:25 +0545

    new host

--- server/config/production.ts
+++ server/config/production.ts
@@ -3,7 +3,7 @@ export default {
   mongoUrl:
     "mongodb+srv://sahsanjeev42:jdzvwLNfiIbxSmiA@uchat-cluster.zdx5bcp.mongodb.net/?retryWrites=true&w=majority&appName=uchat-cluster",
   corsOptions: {
-    origin: "https://u-chat-x43i.onrender.com",
+    origin: "https://u-chat-woad.vercel.app/",
     credentials: true,
   },
   saltFactor: 10,
