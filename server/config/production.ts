f6798734635820d4a32f43c97b7e375c21559978
sanjeevshah1 <sahsanjeev42@gmail.com>
2025-06-20 12:00:56 +0545

    cors

--- server/config/production.ts
+++ server/config/production.ts
@@ -3,7 +3,7 @@ export default {
   mongoUrl:
     "mongodb+srv://sahsanjeev42:jdzvwLNfiIbxSmiA@uchat-cluster.zdx5bcp.mongodb.net/?retryWrites=true&w=majority&appName=uchat-cluster",
   corsOptions: {
-    origin: "https://u-chat-woad.vercel.app/",
+    origin: "https://u-chat-project.vercel.app/",
     credentials: true,
   },
   saltFactor: 10,
